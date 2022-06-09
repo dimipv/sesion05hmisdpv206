f35623c9ab6d7ec5e75e0bc1265fe4cf4183a77e
dimipv <dpv206@inlumine.ual.es>
2022-06-09 11:16:02 +0200

    Ejercicio6

---
+++ sesion05Junitdpv206/test/ual/hmis/sesion05/Ejercicio6test.java
@@ -0,0 +1,14 @@
+package ual.hmis.sesion05;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Ejercicio6test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
